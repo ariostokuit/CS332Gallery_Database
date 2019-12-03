d453b673dea4b18aad1bf2fcfbc29ebbfbd8506f
Ariosto Kuit <Ariostokuitak@csu.fullerton.edu>
2019-12-02 22:29:59 -0800

    Edited Main.cpp, added a switch case statement

--- main.cpp
+++ main.cpp
@@ -6,14 +6,38 @@
 * Application can print records from the DB, access and records based on attribute values
 * Sort a report.
 */
+
+
+
 #include <iostream>
 #include <string>
 using namespace std;
 
+void displayOptions()
+{
+    cout << "1. Print record from the DB\n";
+    cout << "2. Access an yrecord based on attribute values\n";
+    cout << "3. Sort a report\n";
+}
+
 int main()
 {
+    int option;
+    char input = 'n';
+
+	do{
+        displayOptions();
+        cin >> option;
+
+        switch(option)
+        {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+        }
 
 
+    }while(input == 'y' || input == 'Y');
 
     return 0;
 }
