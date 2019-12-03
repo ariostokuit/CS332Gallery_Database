ac5a408f27ccc357c6deffeb5cf76f9155dac1f4
Ariosto Kuit <Ariostokuitak@csu.fullerton.edu>
2019-12-02 21:47:47 -0800

    Added a main.cpp file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+* Ariosto Kuit and Tony Nyugen
+* CS 332 Assignment #5
+*
+* Create an application that can access gallery database using C++.
+* Application can print records from the DB, access and records based on attribute values
+* Sort a report.
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}
